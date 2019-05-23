b61950968dfd357964f3808a2b4e2340ccec2c61
Kseniya Tulikova <ksenia77383@gmail.com>
2019-05-23 16:57:45 +0300

    My first task(IShop)

--- ishop/MyComponent.js
+++ ishop/MyComponent.js
@@ -1,10 +1,50 @@
 let MyComponent = React.createClass({
-    displayName: 'MyComponent' ,
+    displayName: 'MyComponent',
+
+    getDefaultProps : function() {
+        return { nameOfShop: 'We have changed our name before you came, sorry!'}
+    },
+
+    propTypes : {
+        products: React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+                name: React.PropTypes.string,
+                price: React.PropTypes.string,
+                quantity: React.PropTypes.number.isRequired,
+                url: React.PropTypes.string,
+            })
+        ),
+        nameOfShop: React.PropTypes.string.isRequired,
+    },
 
     render: function() {
-        return React.DOM.div( {className : 'MyComponentIShop'},
-            React.DOM.h1( null, 'Всем привет!'),
-            React.DOM.div( {className: 'IShopText'}, "Get Started!")
-        );
+        let productList = [];   
+
+        function renderProduct(item) {
+            let product = 
+                React.DOM.div( {className: [item.name, "product-item"].join(' '), key: item.id},
+                    React.DOM.img({className: 'img', src: `img/${item.src_name}.png`, alt: 'Photo of Mobile'}, ),
+                    React.DOM.h2({className: 'productName'}, item.name),
+                    React.DOM.div({className: 'description'}, 
+                        React.DOM.div({className: 'Price'},
+                            React.DOM.h3(null, 'Price:'),
+                            React.DOM.div(null, item.price)
+                        ),
+                        React.DOM.div({className: 'Quantity'}, 
+                            React.DOM.h3(null, 'Quantity:'),
+                            React.DOM.div(null, item.quantity) 
+                        ),
+                    ),
+    
+                );
+            productList.push(product);            
+        };
+
+        this.props.products.forEach(renderProduct);
+
+        return React.DOM.div({className: 'ProductsList'},
+            React.DOM.div({className: 'products'}, productList),
+            );
     },
+
 });
